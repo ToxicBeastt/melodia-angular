9da8a170fae45e7e05e01308ed7c7642aa086d60
Rayhan <rayhan.aulia@radyalabs.com>
2024-06-30 17:32:52 +0700

    feat: add color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors: {
+      // Background
+      'midnight-black': '#0D0D0D',
+      // Accents and buttons
+      'crimson-red': '#D32F2F',
+      // Highlights and call-to-action
+      gold: '#FFC107',
+      // Text and secondary elements
+      'slate-gray': '#607D8B',
+      // Main text and contrast
+      'ivory-white': '#FAFAFA',
+    },
     extend: {},
   },
   plugins: [],
