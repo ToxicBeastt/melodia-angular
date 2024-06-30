8d6a0f3b1c185185a63b0645390c0f6cdb25cbde
Rayhan <rayhan.aulia@radyalabs.com>
2024-06-30 17:42:52 +0700

    feat: add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {HeaderComponent} from "./layout/header/header.component";
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HeaderComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
