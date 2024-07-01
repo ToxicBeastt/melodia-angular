11d57a48bae9e6b7d44306782f00990fc5586185
Rayhan <rayhan.aulia@radyalabs.com>
2024-07-01 10:16:28 +0700

    fix: update color pallet tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,28 @@ module.exports = {
   ],
   theme: {
     colors: {
-      // Background
-      'midnight-black': '#0D0D0D',
-      // Accents and buttons
-      'crimson-red': '#D32F2F',
-      // Highlights and call-to-action
-      gold: '#FFC107',
-      // Text and secondary elements
-      'slate-gray': '#607D8B',
-      // Main text and contrast
-      'ivory-white': '#FAFAFA',
+      primary: {
+        100: '#F7CAC9',
+        200: '#F5B7B5',
+        300: '#F3A8A4',
+        400: '#F1948F',
+        500: '#A91D3A',
+        600: '#C73659',
+        700: '#D2556F',
+        800: '#E2787F',
+        900: '#F95C83',
+      },
+      secondary: {
+        100: '#F7F7F7',
+        200: '#E5E5E5',
+        300: '#CCCCCC',
+        400: '#B3B3B3',
+        500: '#151515',
+        600: '#333333',
+        700: '#4C4C4C',
+        800: '#666666',
+        900: '#888888',
+      },
     },
     extend: {},
   },
